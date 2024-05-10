94d8e21bde735783230b86a83c2581f47e22788e
Nikita <hrynts2002@gmail.com>
2024-05-10 06:29:21 +0300

    Completed all tasks

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if ((character >= 65 && character <= 90)
+                || (character >= 97 && character <= 122)) {
+            if (character == 'e' || character == 'i' || character == 'o' || character == 'u' || character == 'a') {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        }
+        if (salary <= 10000 && salary >= 0) {
+            System.out.println(salary * 0.85f);
+        }
+        if (10000 < salary && salary <= 20000) {
+            System.out.println(salary * 0.82f);
+        }
+        if (salary > 20000) {
+            System.out.println(salary * 0.8f);
+        }
     }
 }
